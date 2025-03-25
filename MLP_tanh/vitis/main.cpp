2a7acf90031f77805e5da53cd9873f8953e1ab56
Rayleigh47 <yanming8870@outlook.com>
2025-03-25 19:46:46 +0800

    no pragma to meet timing naive implementation

--- MLP_tanh/vitis/main.cpp
+++ MLP_tanh/vitis/main.cpp
@@ -102,9 +102,8 @@ void write_stream(hls::stream<axi_stream> &out_stream, int label) {
 // Compute first hidden layer with quantized ReLU activation.
 void compute_layer0(fixed_t input[INPUT_SIZE],
                     fixed_t hidden1[HIDDEN_SIZE],
-                    const weights_t w0[HIDDEN_SIZE][INPUT_SIZE],
-                    const fixed_t b0[HIDDEN_SIZE]) {
-    #pragma HLS PIPELINE II=2
+                    weights_t w0[HIDDEN_SIZE][INPUT_SIZE],
+                    fixed_t b0[HIDDEN_SIZE]) {
     for (int i = 0; i < HIDDEN_SIZE; i++) {
         fixed_t sum = b0[i];
         for (int j = 0; j < INPUT_SIZE; j++) {
@@ -115,11 +114,10 @@ void compute_layer0(fixed_t input[INPUT_SIZE],
 }
 
 // Compute second hidden layer with quantized ReLU activation.
-void compute_layer1(const fixed_t hidden1[HIDDEN_SIZE],
+void compute_layer1(fixed_t hidden1[HIDDEN_SIZE],
                     fixed_t hidden2[HIDDEN_SIZE],
-                    const weights_t w1[HIDDEN_SIZE][HIDDEN_SIZE],
-                    const fixed_t b1[HIDDEN_SIZE]) {
-    #pragma HLS PIPELINE II=2
+                    weights_t w1[HIDDEN_SIZE][HIDDEN_SIZE],
+                    fixed_t b1[HIDDEN_SIZE]) {
     for (int i = 0; i < HIDDEN_SIZE; i++) {
         fixed_t sum = b1[i];
         for (int j = 0; j < HIDDEN_SIZE; j++) {
@@ -130,11 +128,10 @@ void compute_layer1(const fixed_t hidden1[HIDDEN_SIZE],
 }
 
 // Compute output layer (no activation quantization applied here)
-void compute_layer2(const fixed_t hidden2[HIDDEN_SIZE],
+void compute_layer2(fixed_t hidden2[HIDDEN_SIZE],
                     fixed_t output[OUTPUT_SIZE],
-                    const weights_t w2[OUTPUT_SIZE][HIDDEN_SIZE],
-                    const fixed_t b2[OUTPUT_SIZE]) {
-    #pragma HLS PIPELINE II=2
+                    weights_t w2[OUTPUT_SIZE][HIDDEN_SIZE],
+                    fixed_t b2[OUTPUT_SIZE]) {
     for (int i = 0; i < OUTPUT_SIZE; i++) {
         fixed_t sum = b2[i];
         for (int j = 0; j < HIDDEN_SIZE; j++) {
@@ -144,8 +141,8 @@ void compute_layer2(const fixed_t hidden2[HIDDEN_SIZE],
     }
 }
 
-void compute_softmax(fixed_t output[OUTPUT_SIZE], float class_predictions[OUTPUT_SIZE]) {
-    // Rescale the output layer to float
+void compute_softmax(fixed_t output[OUTPUT_SIZE], fixed_t class_predictions[OUTPUT_SIZE]) {
+    // Use fixed_t
     fixed_t max_val = output[0];
     for (int i = 1; i < OUTPUT_SIZE; i++) {
         if (output[i] > max_val) {
@@ -153,10 +150,10 @@ void compute_softmax(fixed_t output[OUTPUT_SIZE], float class_predictions[OUTPUT
         }
     }
 
-    float sum = 0.0f;
+    fixed_t sum = 0.0f;
     for (int i = 0; i < OUTPUT_SIZE; i++) {
-        float temp = (float)(output[i] - max_val);
-        float exp_val = hls::expf(temp);
+        fixed_t temp = (fixed_t)(output[i] - max_val);
+        fixed_t exp_val = hls::expf(temp);
         class_predictions[i] = exp_val;
         sum += exp_val;
     }
@@ -183,39 +180,39 @@ void mlp_tanh_forward(hls::stream<axi_stream> &in_stream,
     static fixed_t network_2_bias[HIDDEN_SIZE];
     static fixed_t network_4_bias[OUTPUT_SIZE];
 
-    // Place weight/bias arrays in LUTRAM/BRAM
-    #pragma HLS BIND_STORAGE variable=network_0_weight type=RAM_2P impl=BRAM
-    #pragma HLS BIND_STORAGE variable=network_2_weight type=RAM_2P impl=BRAM
-    #pragma HLS BIND_STORAGE variable=network_4_weight type=RAM_2P impl=BRAM
-    #pragma HLS BIND_STORAGE variable=network_0_bias type=RAM_1P impl=LUTRAM
-    #pragma HLS BIND_STORAGE variable=network_2_bias type=RAM_1P impl=LUTRAM
-    #pragma HLS BIND_STORAGE variable=network_4_bias type=RAM_1P impl=LUTRAM
+    // // Place weight/bias arrays in LUTRAM/BRAM
+    // #pragma HLS BIND_STORAGE variable=network_0_weight type=RAM_2P impl=BRAM
+    // #pragma HLS BIND_STORAGE variable=network_2_weight type=RAM_2P impl=BRAM
+    // #pragma HLS BIND_STORAGE variable=network_4_weight type=RAM_2P impl=BRAM
+    // #pragma HLS BIND_STORAGE variable=network_0_bias type=RAM_1P impl=LUTRAM
+    // #pragma HLS BIND_STORAGE variable=network_2_bias type=RAM_1P impl=LUTRAM
+    // #pragma HLS BIND_STORAGE variable=network_4_bias type=RAM_1P impl=LUTRAM
 
-    // Partition weight and bias arrays to allow parallel accesses
-    #pragma HLS ARRAY_PARTITION variable=network_0_weight complete dim=1
-    #pragma HLS ARRAY_PARTITION variable=network_2_weight complete dim=1
-    #pragma HLS ARRAY_PARTITION variable=network_4_weight complete dim=1
-    #pragma HLS ARRAY_PARTITION variable=network_0_bias complete
-    #pragma HLS ARRAY_PARTITION variable=network_2_bias complete
-    #pragma HLS ARRAY_PARTITION variable=network_4_bias complete
+    // // Partition weight and bias arrays to allow parallel accesses
+    // #pragma HLS ARRAY_PARTITION variable=network_0_weight complete dim=1
+    // #pragma HLS ARRAY_PARTITION variable=network_2_weight complete dim=1
+    // #pragma HLS ARRAY_PARTITION variable=network_4_weight complete dim=1
+    // #pragma HLS ARRAY_PARTITION variable=network_0_bias complete
+    // #pragma HLS ARRAY_PARTITION variable=network_2_bias complete
+    // #pragma HLS ARRAY_PARTITION variable=network_4_bias complete
 
     fixed_t hidden1[HIDDEN_SIZE];
     fixed_t hidden2[HIDDEN_SIZE];
     fixed_t input[INPUT_SIZE];
     fixed_t output[OUTPUT_SIZE];
-    float class_predictions[OUTPUT_SIZE];
+    fixed_t class_predictions[OUTPUT_SIZE];
 
-    // Place hidden, input and output layers in BRAM
-    #pragma HLS BIND_STORAGE variable=hidden1 type=RAM_1P impl=LUTRAM
-    #pragma HLS BIND_STORAGE variable=hidden2 type=RAM_1P impl=LUTRAM
-    #pragma HLS BIND_STORAGE variable=input type=RAM_1P impl=LUTRAM
-    #pragma HLS BIND_STORAGE variable=output type=RAM_1P impl=LUTRAM
+    // // Place hidden, input and output layers in BRAM
+    // #pragma HLS BIND_STORAGE variable=hidden1 type=RAM_1P impl=LUTRAM
+    // #pragma HLS BIND_STORAGE variable=hidden2 type=RAM_1P impl=LUTRAM
+    // #pragma HLS BIND_STORAGE variable=input type=RAM_1P impl=LUTRAM
+    // #pragma HLS BIND_STORAGE variable=output type=RAM_1P impl=LUTRAM
 
-    // Partition arrays to allow parallel accesses
-    #pragma HLS ARRAY_PARTITION variable=hidden1 complete
-    #pragma HLS ARRAY_PARTITION variable=hidden2 complete
-    #pragma HLS ARRAY_PARTITION variable=input complete
-    #pragma HLS ARRAY_PARTITION variable=output complete
+    // // Partition arrays to allow parallel accesses
+    // #pragma HLS ARRAY_PARTITION variable=hidden1 complete
+    // #pragma HLS ARRAY_PARTITION variable=hidden2 complete
+    // #pragma HLS ARRAY_PARTITION variable=input complete
+    // #pragma HLS ARRAY_PARTITION variable=output complete
 
     if (mode == 1) {
        // Store weights & biases
@@ -231,7 +228,7 @@ void mlp_tanh_forward(hls::stream<axi_stream> &in_stream,
 
         // Determine the predicted label
         int label = 0;
-        float max_val = class_predictions[0];
+        fixed_t max_val = class_predictions[0];
         for (int i = 1; i < OUTPUT_SIZE; i++) {
             if (class_predictions[i] > max_val) {
                 max_val = class_predictions[i];
