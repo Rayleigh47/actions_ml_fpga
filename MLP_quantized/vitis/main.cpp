4607a760e7c76f0220a46259a91cb37c4b95b3d3
Rayleigh47 <yanming8870@outlook.com>
2025-03-28 21:27:03 +0800

    merged working vitis code back to MLP_quantized folder

--- MLP_quantized/vitis/main.cpp
+++ MLP_quantized/vitis/main.cpp
@@ -1,11 +1,147 @@
 #include "model_params.hpp"
 #include <hls_stream.h>
 #include <math.h>
-#include<hls_math.h>
+#include <hls_math.h>
 #include <ap_int.h>
 
-// Define a helper function for 8-bit quantized ReLU.
-// It scales the 32-bit accumulator, rounds, and saturates to the range [0, 127].
+// Contiguous RAM to store DMA transfer
+static int32_t total_weights_bias[TOTAL_WEIGHTS_BIAS] = {0};
+// Weights and biases
+static int8_t weights_0[HIDDEN_SIZE][INPUT_SIZE] = {0};
+static int32_t bias_0[HIDDEN_SIZE] = {0};
+static int8_t weights_1[HIDDEN_SIZE][HIDDEN_SIZE] = {0};
+static int32_t bias_1[HIDDEN_SIZE] = {0};
+static int8_t weights_2[OUTPUT_SIZE][HIDDEN_SIZE] = {0};
+static int32_t bias_2[OUTPUT_SIZE] = {0};
+
+void read_weights_biases(hls::stream<axi_stream> &in_stream) {
+    axi_stream inVal;
+    for (int i = 0; i < TOTAL_WEIGHTS_BIAS; i++) {
+        #pragma HLS PIPELINE off
+        inVal = in_stream.read();
+        int32_t val = inVal.data;
+        total_weights_bias[i] = val;
+    }
+}
+
+void init_layers() {
+    int index = 0;
+    // Load first layer weights
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        for (int j = 0; j < INPUT_SIZE; j++) {
+            weights_0[i][j] = total_weights_bias[index++];
+        }
+    }
+    // Load first layer biases
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        bias_0[i] = total_weights_bias[index++];
+    }
+    // Load second layer weights
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        for (int j = 0; j < HIDDEN_SIZE; j++) {
+            weights_1[i][j] = total_weights_bias[index++];
+        }
+    }
+    // Load second layer biases
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        bias_1[i] = total_weights_bias[index++];
+    }
+    // Load third layer weights
+    for (int i = 0; i < OUTPUT_SIZE; i++) {
+        for (int j = 0; j < HIDDEN_SIZE; j++) {
+            weights_2[i][j] = total_weights_bias[index++];
+        }
+    }
+    // Load third layer biases
+    for (int i = 0; i < OUTPUT_SIZE; i++) {
+        bias_2[i] = total_weights_bias[index++];
+    }
+}
+
+void write_weights_biases(hls::stream<axi_stream> &out_stream) {
+    axi_stream outVal;
+    // Write first layer weights
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        #pragma HLS PIPELINE OFF
+        for (int j = 0; j < INPUT_SIZE; j++) {
+            #pragma HLS PIPELINE OFF
+            outVal.data = weights_0[i][j];
+            outVal.keep = -1;
+            outVal.strb = -1;
+            outVal.last = 0;
+            out_stream.write(outVal);
+        }
+    }
+    // Write first layer biases
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        #pragma HLS PIPELINE OFF
+        outVal.data = bias_0[i];
+        outVal.keep = -1;
+        outVal.strb = -1;
+        outVal.last = 0;
+        out_stream.write(outVal);
+    }
+    // Write second layer weights
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        #pragma HLS PIPELINE OFF
+        for (int j = 0; j < HIDDEN_SIZE; j++) {
+            #pragma HLS PIPELINE OFF
+            outVal.data = weights_1[i][j];
+            outVal.keep = -1;
+            outVal.strb = -1;
+            outVal.last = 0;
+            out_stream.write(outVal);
+        }
+    }
+    // Write second layer biases
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        #pragma HLS PIPELINE OFF
+        outVal.data = bias_1[i];
+        outVal.keep = -1;
+        outVal.strb = -1;
+        outVal.last = 0;
+        out_stream.write(outVal);
+    }
+    // Write third layer weights
+    for (int i = 0; i < OUTPUT_SIZE; i++) {
+        #pragma HLS PIPELINE OFF
+        for (int j = 0; j < HIDDEN_SIZE; j++) {
+            #pragma HLS PIPELINE OFF
+            outVal.data = weights_2[i][j];
+            outVal.keep = -1;
+            outVal.strb = -1;
+            outVal.last = 0;
+            out_stream.write(outVal);
+        }
+    }
+    // Write third layer biases
+    for (int i = 0; i < OUTPUT_SIZE; i++) {
+        #pragma HLS PIPELINE OFF
+        outVal.data = bias_2[i];
+        outVal.keep = -1;
+        outVal.strb = -1;
+        outVal.last = (i == OUTPUT_SIZE - 1 ? 1 : 0);
+        out_stream.write(outVal);
+    }
+}
+
+void read_inputs(hls::stream<axi_stream> &in_stream, int8_t input[INPUT_SIZE]) {
+    data x;
+    axi_stream inVal;
+    for (int i = 0; i < INPUT_SIZE; i++) {
+        inVal = in_stream.read();
+        x.intVal = inVal.data;
+        float scaled = x.floatVal * 127.0f;
+        if (scaled > 127.0f) {
+            scaled = 127.0f;
+        }
+        if (scaled < -128.0f) {
+            scaled = -128.0f;
+        }
+        input[i] = (int8_t) (scaled + 0.5f);
+    }
+}
+
 int8_t quantize_relu(int32_t sum, float scale) {
     float result = sum * scale;
     int result_int = (int)(result + 0.5f);
@@ -14,75 +150,65 @@ int8_t quantize_relu(int32_t sum, float scale) {
     return (int8_t)result_int;
 }
 
-// Read AXI Stream and store into input array as int8_t
-// Here, we assume the incoming float is in [0,1] and we scale it to int8_t using a factor of 127.
-void read_stream(hls::stream<axi_stream> &in_stream, int8_t input[INPUT_SIZE]) {
-    data x;
-    axi_stream inVal;
-    for (int i = 0; i < INPUT_SIZE; i++) {
-        #pragma HLS PIPELINE II=1
-        inVal = in_stream.read();
-        x.intVal = inVal.data;  // reinterpret the 32-bit data as a float
-        float scaled = x.floatVal * 127.0f;
-        if (scaled > 127.0f) scaled = 127.0f;
-        if (scaled < -128.0f) scaled = -128.0f;
-        input[i] = (int8_t)(scaled + 0.5f); // round by adding 0.5
-    }
+void inference(int8_t input[INPUT_SIZE],
+               int8_t hidden_1[HIDDEN_SIZE],
+               int8_t hidden_2[HIDDEN_SIZE],
+               int32_t output[OUTPUT_SIZE],
+               int8_t weights_0[HIDDEN_SIZE][INPUT_SIZE],
+               int8_t weights_1[HIDDEN_SIZE][HIDDEN_SIZE],
+               int8_t weights_2[OUTPUT_SIZE][HIDDEN_SIZE],
+               int32_t bias_0[HIDDEN_SIZE],
+               int32_t bias_1[HIDDEN_SIZE],
+               int32_t bias_2[OUTPUT_SIZE],
+               float scale_0,
+               float scale_1,
+               float class_predictions[OUTPUT_SIZE]) {
+    compute_layer0(input, hidden_1, weights_0, bias_0, scale_0);
+    compute_layer1(hidden_1, hidden_2, weights_1, bias_1, scale_1);
+    compute_layer2(hidden_2, output, weights_2, bias_2);
+    compute_softmax(output, class_predictions);
 }
 
-// Write the predicted label to the AXI stream
-void write_stream(hls::stream<axi_stream> &out_stream, int label) {
-    axi_stream outVal;
-    outVal.data = label;
-    outVal.keep = -1;
-    outVal.strb = -1;
-    outVal.last = 1;
-    out_stream.write(outVal);
-}
-
-// Compute first hidden layer with quantized ReLU activation.
 void compute_layer0(int8_t input[INPUT_SIZE],
                     int8_t hidden1[HIDDEN_SIZE],
-                    const int8_t w0[HIDDEN_SIZE][INPUT_SIZE],
-                    const int32_t b0[HIDDEN_SIZE],
-                    const float scale0) {
+                    int8_t w0[HIDDEN_SIZE][INPUT_SIZE],
+                    int32_t b0[HIDDEN_SIZE],
+                    float scale_0) {
     for (int i = 0; i < HIDDEN_SIZE; i++) {
-        #pragma HLS PIPELINE II=1
+        #pragma HLS PIPELINE OFF
         int32_t sum = b0[i];
         for (int j = 0; j < INPUT_SIZE; j++) {
             #pragma HLS UNROLL
             int32_t product = input[j] * w0[i][j];
             sum += product;
         }
-        hidden1[i] = quantize_relu(sum, scale0);
+        hidden1[i] = quantize_relu(sum, scale_0);
     }
 }
 
-// Compute second hidden layer with quantized ReLU activation.
 void compute_layer1(int8_t hidden1[HIDDEN_SIZE],
                     int8_t hidden2[HIDDEN_SIZE],
-                    const int8_t w1[HIDDEN_SIZE][HIDDEN_SIZE],
-                    const int32_t b1[HIDDEN_SIZE],
-                    const float scale1) {
+                    int8_t w1[HIDDEN_SIZE][HIDDEN_SIZE],
+                    int32_t b1[HIDDEN_SIZE],
+                    float scale_1) {
     for (int i = 0; i < HIDDEN_SIZE; i++) {
-        #pragma HLS PIPELINE II=1
+        #pragma HLS PIPELINE OFF
         int32_t sum = b1[i];
         for (int j = 0; j < HIDDEN_SIZE; j++) {
             #pragma HLS UNROLL
             int32_t product = hidden1[j] * w1[i][j];
             sum += product;
         }
-        hidden2[i] = quantize_relu(sum, scale1);
+        hidden2[i] = quantize_relu(sum, scale_1);
     }
 }
 
-// Compute output layer (no activation quantization applied here)
 void compute_layer2(int8_t hidden2[HIDDEN_SIZE],
                     int32_t output[OUTPUT_SIZE],
-                    const int8_t w2[OUTPUT_SIZE][HIDDEN_SIZE],
-                    const int32_t b2[OUTPUT_SIZE]) {
+                    int8_t w2[OUTPUT_SIZE][HIDDEN_SIZE],
+                    int32_t b2[OUTPUT_SIZE]) {
     for (int i = 0; i < OUTPUT_SIZE; i++) {
-        #pragma HLS PIPELINE II=1
+        #pragma HLS PIPELINE OFF
         int32_t sum = b2[i];
         for (int j = 0; j < HIDDEN_SIZE; j++) {
             #pragma HLS UNROLL
@@ -117,37 +243,8 @@ void compute_softmax(int32_t output[OUTPUT_SIZE], float class_predictions[OUTPUT
     }
 }
 
-// Top-level function
-void mlp_quantized_72_forward(hls::stream<axi_stream> &in_stream, hls::stream<axi_stream> &out_stream) {
-    #pragma HLS INTERFACE axis port=in_stream
-    #pragma HLS INTERFACE axis port=out_stream
-    #pragma HLS INTERFACE s_axilite port=return
-
-    // Bind weights and biases to BRAM/LUTRAM
-    #pragma HLS BIND_STORAGE variable=weight_layer_0 type=ROM_2P impl=BRAM
-    #pragma HLS BIND_STORAGE variable=weight_layer_1 type=ROM_2P impl=LUTRAM
-    #pragma HLS BIND_STORAGE variable=weight_layer_2 type=ROM_2P impl=LUTRAM
-    #pragma HLS BIND_STORAGE variable=bias_layer_0   type=ROM_1P impl=BRAM
-    #pragma HLS BIND_STORAGE variable=bias_layer_1   type=ROM_1P impl=BRAM
-    #pragma HLS BIND_STORAGE variable=bias_layer_2   type=ROM_1P impl=BRAM
-
-    int8_t input[INPUT_SIZE];
-    int8_t hidden1[HIDDEN_SIZE];
-    int8_t hidden2[HIDDEN_SIZE];
-    int32_t output[OUTPUT_SIZE];
-    float class_predictions[OUTPUT_SIZE];
-
-    // Scaling factors for quantization (tune these based on calibration)
-    const float scale0 = 0.000030655f / 0.015625f; // From ONNX file
-    const float scale1 = 0.000061035f / 0.015625f; // From ONNX file
-
-    read_stream(in_stream, input);
-    compute_layer0(input, hidden1, weight_layer_0, bias_layer_0, scale0);
-    compute_layer1(hidden1, hidden2, weight_layer_1, bias_layer_1, scale1);
-    compute_layer2(hidden2, output, weight_layer_2, bias_layer_2);
-    compute_softmax(output, class_predictions);
-
-    float tmp = 0.9f;
+void send_outputs(hls::stream<axi_stream> &out_stream, float class_predictions[OUTPUT_SIZE]) {
+    float tmp = 0.8f; // 80% threshold
     int label = 0;
     for (int i = 0; i < OUTPUT_SIZE; i++) {
         #pragma HLS PIPELINE II=1
@@ -156,5 +253,39 @@ void mlp_quantized_72_forward(hls::stream<axi_stream> &in_stream, hls::stream<ax
             label = i;
         }
     }
-    write_stream(out_stream, label);
+    axi_stream outVal;
+    outVal.data = label;
+    outVal.keep = -1;
+    outVal.strb = -1;
+    outVal.last = 1;
+    out_stream.write(outVal);
+}
+
+// Top-level function
+void mlp_quantized(hls::stream<axi_stream> &in_stream, hls::stream<axi_stream> &out_stream) {
+    // Instantiate top level interface
+    #pragma HLS INTERFACE axis port=in_stream
+    #pragma HLS INTERFACE axis port=out_stream
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
+
+    int8_t input[INPUT_SIZE];
+    int8_t hidden_1[HIDDEN_SIZE];
+    int8_t hidden_2[HIDDEN_SIZE];
+    int32_t output[OUTPUT_SIZE];
+    float class_predictions[OUTPUT_SIZE];
+
+    // Scaling factors for quantization (tune these based on calibration)
+    const float scale_0 = 0.000030655f / 0.015625f; // From ONNX file
+    const float scale_1 = 0.000061035f / 0.015625f; // From ONNX file
+
+    
+    read_weights_biases(in_stream);
+    init_layers();
+    // write_weights_biases(out_stream);
+    for (int i = 0; i < 1000; i ++) {
+        read_inputs(in_stream, input);
+        inference(input, hidden_1, hidden_2, output, weights_0, weights_1, weights_2, bias_0, bias_1, bias_2, scale_0, scale_1, class_predictions);
+        send_outputs(out_stream, class_predictions);
+    }
+
 }
--- MLP_quantized/vitis/test.cpp
+++ MLP_quantized/vitis/test.cpp
@@ -4,30 +4,45 @@
 #include "model_params.hpp"
 #include "test_data.hpp"
 
-typedef ap_axis<32, 2, 5, 6> axi_stream;
-
-// Utility function to check if two floats are approximately equal
-//bool is_approx_equal(float a, float b, float tolerance = 0.00001f) {
-//    return std::fabs(a - b) <= tolerance;
-//}
-
 bool is_equal(int a, int b) {
     return (a-b) == 0;
 }
 
 int main() {
+    // Load the weights and biases into the model
+    hls::stream<axi_stream> in_stream;
+    hls::stream<axi_stream> out_stream;
+    for (int i = 0; i < length; i++) {
+        axi_stream val;
+        val.data = weights_and_bias[i];
+        val.last = (i == length - 1) ? 1 : 0;
+        in_stream.write(val);
+    }
+    // mlp_quantized(in_stream, out_stream);
+
+    // // LOAD AND WRITE
+    // mlp_quantized(in_stream, out_stream);
+
+    // int weights_and_bias_out[length];
+    // int c = 0;
+    // for (int i = 0; i < length; i++) {
+    //     axi_stream val = out_stream.read();
+    //     weights_and_bias_out[i] = val.data;
+    //     if (weights_and_bias_out[i] == weights_and_bias[i]) {
+    //         c += 1;
+    //     }
+    // }
+    // std::cout << "Count =" << c <<std::endl;
+
+
     // Results storage
     // float model_output[test_length][OUTPUT_SIZE];
+
     int model_output[test_length];
     bool all_tests_passed = true;
     int count = 0;
-
     // Process each test input
     for (int i = 0; i < test_length; i++) {
-        // Create input and output streams
-        hls::stream<axi_stream> in_stream;
-        hls::stream<axi_stream> out_stream;
-
         // Prepare input data in AXI format
         for (int j = 0; j < INPUT_SIZE; j++) {
             axi_stream val;
@@ -37,7 +52,7 @@ int main() {
         }
 
         // Run the neural network
-        mlp_quantized_72_forward(in_stream, out_stream);
+        mlp_quantized(in_stream, out_stream);
 
         axi_stream val = out_stream.read();
         model_output[i] = val.data;
@@ -45,38 +60,7 @@ int main() {
         if (is_equal(test_labels[i], model_output[i])) {
         	count += 1;
         }
-        // std::cout << "Expected " << test_labels[i] << ", got " << model_output[i] << std::endl;
-
-//        // Get output data
-//        for (int j = 0; j < OUTPUT_SIZE; j++) {
-//            AXI_VAL val = out_stream.read();
-//            model_output[i][j] = *((float*)&val.data);
-//        }
-//
-//        // Verify results for this test case
-//        bool test_passed = true;
-//        for (int j = 0; j < OUTPUT_SIZE; j++) {
-//            if (!is_approx_equal(test_output[i][j], model_output[i][j])) {
-//                test_passed = false;
-//                all_tests_passed = false;
-//                std::cout << "Test " << i << " failed at output " << j << ": ";
-//                std::cout << "Expected " << test_output[i][j] << ", got " << model_output[i][j] << std::endl;
-//            }
-//        }
-//
-//        if (test_passed) {
-//            std::cout << "Test " << i << " passed." << std::endl;
-//        }
     }
     float accuracy = count / 1000.0 * 100;
     std::cout << "Accuracy =" << accuracy <<std::endl;
-    
-    // // Final test result
-    // if (all_tests_passed) {
-    //     std::cout << "All tests passed!" << std::endl;
-    //     return 0;
-    // } else {
-    //     std::cout << "Some tests failed." << std::endl;
-    //     return 1;
-    // }
 }
